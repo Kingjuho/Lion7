11da7820f27bdce2989e9d9d4176018d1d3fd397
Jhlee <ljh990627@gmail.com>
2026-01-05 10:15:30 +0900

    Kingjuho
    프로젝트 업로드

---
+++ CsharpStudy/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpStudy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 2025/12/31 C# Study #1 */
+            /*
+            int posX = 0, posY = 50, posZ = 100;
+            Console.WriteLine("X = {0}, Y = {1}, Z = {2}", posX, posY, posZ);
+            Console.WriteLine("X = " + posX + ", Y = " + posY + ", Z = " + posZ);
+            Console.WriteLine($"X = {posX}, Y = {posY}, Z = {posZ}");
+
+            //int red = 255, green = 128, blue = 0;
+            //Console.WriteLine("R = {0}, G = {1}, B = {2}", red, green, blue);
+            //Console.WriteLine("R = " + red + ", G = " + green + ", B = " + blue);
+            //Console.WriteLine($"R = {red}, G = {green}, B = {blue}");
+
+            Console.WriteLine("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+            Console.WriteLine("┃  * CONTINUE                            ┃");
+            Console.WriteLine("┃            * Edd                       ┃");
+            Console.WriteLine("┃            * Team Flare Secret HQ      ┃");
+            Console.WriteLine("┃  * Badges:   7      Pokedex:  261      ┃");
+            Console.WriteLine("┃  * Time:          25 : 20              ┃");
+            Console.WriteLine("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛");
+            Console.WriteLine();
+            Console.WriteLine("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+            Console.WriteLine("┃ MYSTERY GIFT                           ┃");
+            Console.WriteLine("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛");
+            Console.WriteLine("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+            Console.WriteLine("┃ LIVE COMPETITION                       ┃");
+            Console.WriteLine("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛");
+            */
+
+
+            /* 2026/01/05 C# Study #2 */
+
+
+        }
+    }
+}
