afa5b6f7db46c31241bd2c6cad48a3b22077c0a3
Jhlee <ljh990627@gmail.com>
2026-01-19 00:53:02 +0900

    야생 포켓몬 레벨 하향조정
    7레벨 꼬렛의 진심 몸통박치기에 파이리가 박살남

--- PokemonTextRPG/Pokemons/PokemonFactory.cs
+++ PokemonTextRPG/Pokemons/PokemonFactory.cs
@@ -17,7 +17,7 @@
         // 야생 포켓몬 생성
         public static Pokemon CreateWildPokemon()
         {
-            int level = Constants.random.Next(2, 8);
+            int level = Constants.random.Next(2, 5);
             int roll = Constants.random.Next(0, 2);
 
             // TODO: 야생 포켓몬이 아직 2마리만 있어서 이런 방식을 쓰지만, 추후 야생 포켓몬만 있는 Id 리스트를 만들어 랜덤으로 뽑게 하는 걸로 리팩토링
